a6d44665a50334691deb6a74f9ad4c752f7db5a5
Steven Wokke <wokste@gmail.com>
2013-07-13 12:45:19 +0200

    Items can now be identified using a key.

--- items/itemdefmanager.cpp
+++ items/itemdefmanager.cpp
@@ -6,13 +6,21 @@
 #include "../utils/texture.h"
 #include <iostream>
 
-ItemDefManager::ItemDefManager(const Game& game, const std::string& configFileName){
-	ConfigNode::load(configFileName, [&] (ConfigNode& configArray){
-		configArray.forEachNode([&] (ConfigNode& config) {
-			items.emplace_back(new ItemType(config));
+ItemDefManager::ItemDefManager(const Game& game, const std::string& jsonFileName){
+	ConfigNode::load(jsonFileName, [&] (ConfigNode& jsonArray){
+		jsonArray.forEachNode([&] (ConfigNode& json) {
+			insert(std::unique_ptr<ItemType>(new ItemType(json)), json.getString("tag",""));
 		});
 	});
 }
 
 ItemDefManager::~ItemDefManager(){
+
+}
+
+int ItemDefManager::insert(std::unique_ptr<ItemType> type, const std::string tag){
+	items.push_back(std::move(type));
+	if(tag != ""){
+		tags.insert(make_pair(tag, (items.size() - 1)));
+	}
 }
--- items/itemdefmanager.h
+++ items/itemdefmanager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <map>
 #include <string>
 #include <memory>
 
@@ -9,10 +10,13 @@ class Game;
 
 class ItemDefManager{
 	std::vector<std::unique_ptr<ItemType>> items;
+	std::map<std::string, int> tags;
 public:
 	ItemDefManager(const Game& game, const std::string& configFileName);
 	~ItemDefManager();
 	ItemType& operator[](int id){return *items[id];}
+	ItemType& operator[](std::string tag){return *items[tags.at(tag)];}
+	int insert(std::unique_ptr<ItemType> type, const std::string tag);
 	int size() {return items.size();}
 	std::unique_ptr<Texture> textureSet;
 };
